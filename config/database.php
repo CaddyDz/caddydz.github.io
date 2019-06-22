a39c4e77e923c91ef6a8c2ac9bb40948c32cd0ec
Caddy <tbitw31@gmail.com>
2019-06-22 21:39:59 +0100

    removed heroku stuff
    Signed-off-by: Caddy <tbitw31@gmail.com>

--- config/database.php
+++ config/database.php
@@ -1,11 +1,5 @@
 <?php
 
-$url = parse_url(env("DATABASE_URL", 'postgres://user:pass@host:5432/db'));
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
-
 return [
 
     /*
@@ -62,13 +56,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $host,
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
