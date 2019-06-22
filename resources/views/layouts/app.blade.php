8341b8faf5785d73e1c5b3c00858175e5714fb13
Caddy <tbitw31@gmail.com>
2019-06-22 21:44:51 +0100

    included raphael cdn in production
    Signed-off-by: Caddy <tbitw31@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -136,9 +136,11 @@
 
     <!-- SCRIPTS -->
 
-    <!-- jQuery -->
     @env('production')
+        <!-- jQuery -->
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <!-- Raphael for livicons -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.4/raphael-min.js" integrity="sha256-Gk+dzc4kV2rqAZMkyy3gcfW6Xd66BhGYjVWa/FjPu+s=" crossorigin="anonymous"></script>
     @else
         <script src="{{ mix('js/vendor.js') }}"></script>
     @endenv
