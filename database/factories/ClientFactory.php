bf44468a2fb6472f777059fc63f0a484eedd86f2
Salim Djerbouh <caddydz4@gmail.com>
2019-08-30 22:03:28 +0100

    specified argument names for faker image
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -4,9 +4,16 @@ use Caddy\Client;
 use Faker\Generator as Faker;
 
 $factory->define(Client::class, function (Faker $faker) {
-    return [
-        'name' => $faker->name,
-        'image' => 'clients/' . $faker->image(storage_path() . '/app/public/clients', 180, 35, 'business', false, false),
-        'url' => $faker->url,
-    ];
+		return [
+			'name' => $faker->name,
+			'image' => 'clients/' . $faker->image(
+				$dir = storage_path() . '/app/public/clients',
+				$width = 180,
+				$height = 35,
+				$category = 'business',
+				$fullPath = false,
+				$randomize = true,
+			),
+			'url' => $faker->url,
+		];
 });
