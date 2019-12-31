aec7aacfd27acfca2bee276bc01e924e90918055
Salim Djerbouh <caddydz4@gmail.com>
2019-12-31 13:54:47 +0100

    remove closure route to optimize for production
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,5 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('getHeroData', 'PagesController@getHeroData');
 Route::get('getNavData', 'PagesController@getNavData');
