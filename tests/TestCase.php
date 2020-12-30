b41f791f1e4fdffc9ab5b5b2a860f6228cd4e771
Salim Djerbouh <caddydz4@gmail.com>
2020-12-30 18:36:32 +0100

    Enabled exceptions reporting globally on tests
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,7 +5,14 @@ declare(strict_types=1);
 namespace Tests;
 
 use Illuminate\Foundation\Testing\{DatabaseMigrations, TestCase as BaseTestCase};
+
 abstract class TestCase extends BaseTestCase
 {
 	use CreatesApplication, DatabaseMigrations;
+
+	public function setUp(): void
+	{
+		parent::setUp();
+		$this->withoutExceptionHandling();
+	}
 }
