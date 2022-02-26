8da347dbc48c56cb40afcb3eb6e22583f906b4f6
Salim Djerbouh <caddydz4@gmail.com>
2022-02-26 20:55:16 +0100

    Avoid redeclaring helper function
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- app/Helpers.php
+++ app/Helpers.php
@@ -2,33 +2,46 @@
 
 declare(strict_types=1);
 
-function sluggify(string $string): string
-{
-	// replace non letter or digits by -
-	$url = preg_replace('~[^\pL\d]+~u', '-', $string);
+/**
+ * Slugify a given string.
+ *
+ * Make a pretty url slug for SEO.
+ *
+ * @param string $string The string to make a url from.
+ *
+ * @return string $url the output slug
+ *
+ * @throws Exception
+ **/
+if (!function_exists('sluggify')) {
+	function sluggify(string $string): string
+	{
+		// replace non letter or digits by -
+		$url = preg_replace('~[^\pL\d]+~u', '-', $string);
 
-	// transliterate
-	try {
-		$url = iconv('utf-8', 'us-ascii//TRANSLIT', $url);
-	} catch (Exception $ex) {
-		return 'n-a';
+		// transliterate
+		try {
+			$url = iconv('utf-8', 'us-ascii//TRANSLIT', $url);
+		} catch (Exception $ex) {
+			return 'n-a';
+		}
+
+		// remove unwanted characters
+		$url = preg_replace('~[^-\w]+~', '', $url);
+
+		// trim
+		$url = trim($url, '-');
+
+		// remove duplicate -
+		$url = preg_replace('~-+~', '-', $url);
+
+		// lowercase
+		$url = strtolower($url);
+
+		if (empty($url)) {
+			return 'n-a';
+		}
+
+		return $url;
 	}
-
-	// remove unwanted characters
-	$url = preg_replace('~[^-\w]+~', '', $url);
-
-	// trim
-	$url = trim($url, '-');
-
-	// remove duplicate -
-	$url = preg_replace('~-+~', '-', $url);
-
-	// lowercase
-	$url = strtolower($url);
-
-	if (empty($url)) {
-		return 'n-a';
-	}
-
-	return $url;
 }
