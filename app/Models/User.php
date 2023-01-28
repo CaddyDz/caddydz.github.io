6493bcc5c09caa024db0f7203162e52e46cab8dc
Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>
2023-01-28 23:18:55 +0100

    Removed Laravel Sanctum
    * We don't provide an API in this project
    
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -37,7 +36,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
  */
 class User extends Authenticatable
 {
-	use HasApiTokens, HasFactory, Notifiable;
+	use HasFactory, Notifiable;
 
 	/**
 	 * The attributes that are mass assignable.
