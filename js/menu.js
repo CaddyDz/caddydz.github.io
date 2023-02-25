a0aa1316a6f7e7e893ac976239b0129cdc349b82
Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>
2023-02-25 06:34:34 +0100

    Improvements
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- js/menu.js
+++ js/menu.js
@@ -1,28 +1,29 @@
 'use strict';
 
-//Anchors
-$(function(){
-    $('a[href^="#"]').click(function(){
+// Anchor tags
+$(function () {
+    $('a[href^="#"]').click(function () {
         var target = $(this).attr('href');
-        $('html, body').animate({scrollTop: $(target).offset().top - 50}, 800);
+        $('html, body').animate({ scrollTop: $(target).offset().top - 50 }, 800);
         return false;
     });
 });
 
-//Fixed-top menu
+// Fixed-top menu
 function fixedHeader() {
     var ww = $(window).scrollTop();
-    if(ww > 0){
+    if (ww > 0) {
         $('.menu').addClass('menu--active');
         $('.mobile-menu').addClass('mobile-menu--active');
-    }else{
+    } else {
         $('.menu').removeClass('menu--active');
         $('.mobile-menu').removeClass('mobile-menu--active');
     }
 }
+// Immediately call
 fixedHeader();
+
 $(window).on('scroll', function () {
+    // Call on window scroll
     fixedHeader();
 });
-
-
